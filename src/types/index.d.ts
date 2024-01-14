56a364bbc56b5517f11a6f052b901da024d763a4
Arya Emami <aryaemami59@yahoo.com>
2024-01-13 18:11:01 -0600

    Remove `react-native-collapsible` from augmentations

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,12 +1,1 @@
-import "react-native-collapsible";
-
-import type { ReactNode } from "react";
-
 declare module "ts-node/register" {}
-
-declare module "react-native-collapsible" {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-  export interface CollapsibleProps {
-    children?: ReactNode;
-  }
-}
